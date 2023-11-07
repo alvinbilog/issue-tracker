7dcabbd4a83014cc7923ab675e653421f283b8e8
Alvin Patrick Bilog <balvinpatrick@yahoo.com>
2023-11-07 12:23:58 -0600

    Refactor: Apply the Single Responsibility Principle

---
+++ app/issues/[id]/EditIssueButton.tsx
@@ -0,0 +1,18 @@
+import { Issue } from '@prisma/client';
+import { Pencil2Icon } from '@radix-ui/react-icons';
+import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
+import React from 'react';
+
+const EditIssueButton = ({ issue }: { issue: Issue }) => {
+  return (
+    <div>
+      <Button>
+        <Pencil2Icon />
+        <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+      </Button>
+    </div>
+  );
+};
+
+export default EditIssueButton;
